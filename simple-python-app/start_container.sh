54bec2a733698ff81dea3c7145571ebd3383cbea
PalashDevOps <palashchimurkar75@gmail.com>
2024-03-17 23:30:01 +0530

    appspec file added

---
+++ simple-python-app/start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# pulling docker image from Docker Hub
+docker pull docker pull palashdevops4499/simple-python-flask-app
+
+# Run the Docker container from the image
+docker run -d -p 5000:5000 palashdevops4499/simple-python-flask-app
+
+echo "Docker Container has been started. Please wait for a few seconds..."
