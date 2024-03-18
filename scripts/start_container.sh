13250f1b94a59c8fdd4c357d7542a3dc33718d53
Palash Chimurkar <106944393+PalashDevOps@users.noreply.github.com>
2024-03-18 10:36:22 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,9 @@
 set -e
 
 # pulling docker image from Docker Hub
-docker pull palashdevops4499/simple-python-flask-app
+docker pull palashdevops4499/flask-app
 
 # Run the Docker container from the image
-docker run -d -p 5000:5000 palashdevops4499/simple-python-flask-app
+docker run -d -p 5000:5000 palashdevops4499/flask-app
 
 echo "Docker Container has been started. Please wait for a few seconds..."
